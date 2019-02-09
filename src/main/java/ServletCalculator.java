baf99a99a06fb4ae076d8364546e12cf34d6d059
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-09 12:40:10 +0200

    calculator cleaned

--- src/main/java/ServletCalculator.java
+++ src/main/java/ServletCalculator.java
@@ -8,26 +8,51 @@ import java.io.PrintWriter;
 public class ServletCalculator extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int a = Integer.parseInt(req.getParameter("a"));
-        int b = Integer.parseInt(req.getParameter("b"));
-        String command = req.getParameter("op");
-        int result=0;
-        String operation="";
-        switch(command){
-            case "sum": result = a+b;
-            operation = "+";
-            break;
-            case "sub": result = a-b;
-                operation = "-";
-            break;
-            case "mult": result = a*b;
-                operation = "*";
-            break;
-            case "div": result = a/b;
-                operation = "/";
-            break;
+        String responseMessage = "";
+
+        try{
+            int a = convertParameter("a",req);
+            int b = convertParameter("b",req);
+            String command = req.getParameter("op");
+            int result=0;
+            String operation="";
+            switch(command){
+                case "sum": result = a+b;
+                    operation = "+";
+                    break;
+                case "sub": result = a-b;
+                    operation = "-";
+                    break;
+                case "mult": result = a*b;
+                    operation = "*";
+                    break;
+                case "div": result = a/b;
+                    operation = "/";
+                    break;
+                default: throw new RuntimeException("Invalid operation");
+            }
+            responseMessage = String.format("%d %s %d = %d", a, operation, b, result);
+        }catch (ArithmeticException e){
+            responseMessage = "You can't divide by zero";
+        }catch (NumberFormatException e){
+            responseMessage = "Integer conversion error";
+        }catch (IllegalStateException e){
+            responseMessage = e.getMessage();
+        }catch (RuntimeException e){
+            responseMessage = e.getMessage();
         }
-        PrintWriter writer = resp.getWriter();
-        writer.printf("%d %s %d = %d", a, operation, b, result);
+        finally {
+            PrintWriter writer = resp.getWriter();
+            writer.println(responseMessage);
+        }
+
+
+    }
+
+    private int convertParameter(String name, HttpServletRequest req){
+        if(req.getParameter(name) == null){
+            throw new IllegalStateException(String.format("Parameter %s missing",name));
+        }
+        return Integer.parseInt(req.getParameter(name));
     }
 }
